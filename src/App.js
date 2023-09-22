d21d1a2eafb2e492e0c4bd0ff21d9f463e35d984
s-singh18 <65569092+s-singh18@users.noreply.github.com>
2023-09-21 23:08:50 -0700

    Adding a post feature

--- src/App.js
+++ src/App.js
@@ -1,36 +1,96 @@
-import React from 'react';
-import { Routes, Route, Outlet, useParams, useNavigate } from 'react-router-dom';
-import { Navigation } from './components/Navigation';
-import { WalletSelectButton } from './components/WalletSelectButton';
-import { ProfileButton } from './components/ProfileButton';
-import { Posts } from './components/Posts';
-import { ProgressSpinner } from './components/ProgressSpinner';
-import { TopicSearch } from './components/TopicSearch';
-import { UserSearch } from './components/UserSearch';
-import './App.css';
+import React from "react";
+import {
+  Routes,
+  Route,
+  Outlet,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { WalletSelectButton } from "./components/WalletSelectButton";
+import { ProfileButton } from "./components/ProfileButton";
+import { Posts } from "./components/Posts";
+import { ProgressSpinner } from "./components/ProgressSpinner";
+import { TopicSearch } from "./components/TopicSearch";
+import { UserSearch } from "./components/UserSearch";
+import { NewPost } from "./components/NewPost";
+import { buildQuery, arweave, createPostInfo } from "./lib/api";
+import "./App.css";
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+async function waitForNewPosts(txid) {
+  let count = 0;
+  let foundPost = null;
+  let posts = [];
+
+  while (!foundPost) {
+    count += 1;
+    console.log(`attempt ${count}`);
+    await delay(2000 * count);
+    posts = await getPostInfos();
+    foundPost = posts.find((p) => p.txid === txid);
+  }
+
+  let i = posts.indexOf(foundPost);
+  posts.unshift(posts.splice(i, 1)[0]);
+  return posts;
+}
 
 async function getPostInfos() {
-  return [];
+  const query = buildQuery();
+  const results = await arweave.api.post("/graphql", query).catch((err) => {
+    console.error("GraphQL query failed");
+    throw new Error(err);
+  });
+  const edges = results.data.data.transactions.edges;
+  console.log(edges);
+  return edges.map((edge) => createPostInfo(edge.node));
 }
 
 const App = () => {
-  
+  const [isWalletConnected, setIsWalletConnected] = React.useState(false);
+  const [postInfos, setPostInfos] = React.useState([]);
+  const [isSearching, setIsSearching] = React.useState(false);
+
+  async function waitForPost(txid) {
+    setIsSearching(true);
+    let posts = await waitForNewPosts(txid);
+    setPostInfos(posts);
+    setIsSearching(false);
+  }
+
   React.useEffect(() => {
-    getPostInfos();
-  }, [])
+    setIsSearching(true);
+    getPostInfos().then((posts) => {
+      setPostInfos(posts);
+      setIsSearching(false);
+    });
+  }, []);
 
   return (
     <div id="app">
       <div id="content">
         <aside>
           <Navigation />
+          <WalletSelectButton
+            setIsConnected={() => setIsWalletConnected(true)}
+          />
+          <ProfileButton isWalletConnected={isWalletConnected} />
         </aside>
         <main>
           <Routes>
-            <Route path="/" name="home" element={
-            <Home 
-
-            />}
+            <Route
+              path="/"
+              name="home"
+              element={
+                <Home
+                  isWalletConnected={isWalletConnected}
+                  isSearching={isSearching}
+                  postInfos={postInfos}
+                  onPostMessage={waitForPost}
+                />
+              }
             />
             <Route path="/topics" element={<Topics />}>
               <Route path="/topics/" element={<TopicSearch />} />
@@ -51,6 +111,12 @@ const Home = (props) => {
   return (
     <>
       <header>Home</header>
+      <NewPost
+        isLoggedIn={props.isWalletConnected}
+        onPostMessage={props.onPostMessage}
+      />
+      {props.isSearching && <ProgressSpinner />}
+      <Posts postInfos={props.postInfos} />
     </>
   );
 };
@@ -81,13 +147,13 @@ const TopicResults = () => {
 
   const onTopicSearch = (topic) => {
     navigate(`/topics/${topic}`);
-  }
+  };
 
   React.useEffect(() => {
     setIsSearching(true);
     setTopicPostInfos([]);
     try {
-      getPostInfos(null,topic).then(posts => { 
+      getPostInfos(null, topic).then((posts) => {
         setTopicPostInfos(posts);
         setIsSearching(false);
       });
@@ -95,15 +161,15 @@ const TopicResults = () => {
       console.logErrorg(error);
       setIsSearching(false);
     }
-  }, [topic])
+  }, [topic]);
   return (
     <>
-    <TopicSearch searchInput={topic} onSearch={onTopicSearch}/>
-    {isSearching && <ProgressSpinner />}
-    <Posts postInfos={topicPostInfos} />
+      <TopicSearch searchInput={topic} onSearch={onTopicSearch} />
+      {isSearching && <ProgressSpinner />}
+      <Posts postInfos={topicPostInfos} />
     </>
-  )
-}
+  );
+};
 
 function UserResults() {
   const [userPostInfos, setUserPostInfos] = React.useState([]);
@@ -113,27 +179,27 @@ function UserResults() {
 
   const onUserSearch = (address) => {
     navigate(`/users/${address}`);
-  }
+  };
 
   React.useEffect(() => {
     setIsSearching(true);
     try {
-      getPostInfos(addr).then(posts => { 
-        setUserPostInfos(posts); 
+      getPostInfos(addr).then((posts) => {
+        setUserPostInfos(posts);
         setIsSearching(false);
       });
     } catch (error) {
       console.logErrorg(error);
       setIsSearching(false);
     }
-  }, [addr])
+  }, [addr]);
   return (
     <>
-    <UserSearch searchInput={addr} onSearch={onUserSearch}/>
-    {isSearching && <ProgressSpinner />}
-    <Posts postInfos={userPostInfos} />
+      <UserSearch searchInput={addr} onSearch={onUserSearch} />
+      {isSearching && <ProgressSpinner />}
+      <Posts postInfos={userPostInfos} />
     </>
   );
-};
+}
 
 export default App;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { maxMessageLength, abbreviateAddress, getPostTime } from '../lib/api';
+import React from "react";
+import { Link } from "react-router-dom";
+import { maxMessageLength, abbreviateAddress, getPostTime } from "../lib/api";
 
 export const Posts = (props) => {
   return (
     <div>
-      {props.postInfos.map(postInfo =>
+      {props.postInfos.map((postInfo) => (
         <PostItem key={postInfo.txid} postInfo={postInfo} />
-      )}
+      ))}
     </div>
-  )
+  );
 };
 
 const PostItem = (props) => {
@@ -17,18 +17,45 @@ const PostItem = (props) => {
   const [statusMessage, setStatusMessage] = React.useState("");
   const [ownerName, setOwnerName] = React.useState("");
   const [ownerHandle, setOwnerHandle] = React.useState("");
-  const [imgSrc, setImgSrc] = React.useState(props.postInfo.imgSrc || 'img_avatar.png');
+  const [imgSrc, setImgSrc] = React.useState(
+    props.postInfo.imgSrc || "img_avatar.png"
+  );
 
   React.useEffect(() => {
+    const getAccountInfo = async () => {
+      setOwnerName(abbreviateAddress(props.postInfo.owner));
+      const info = await props.postInfo.account;
+      setOwnerName(info.handle);
+      if (info.handle[0] == "@") {
+        props.postInfo.imgSrc = info.profile.avatarURL;
+        setImgSrc(info.profile.avatarURL);
+        setOwnerName(info.profile.name);
+        setOwnerHandle(info.handle);
+      }
+    };
+    getAccountInfo();
+
+    const renderTopic = (topic) => {
+      if (topic)
+        return (
+          <Link to={`/topics/${topic}`} className="postTopic">
+            #{topic}
+          </Link>
+        );
+    };
     let newPostMessage = "";
     let newStatus = "";
-    
+
     if (!props.postInfo.message) {
       setStatusMessage("loading...");
       let isCancelled = false;
 
       const getPostMessage = async () => {
-        setPostMessage('s'.repeat(Math.min(Math.max(props.postInfo.length - 75, 0), maxMessageLength)));
+        setPostMessage(
+          "s".repeat(
+            Math.min(Math.max(props.postInfo.length - 75, 0), maxMessageLength)
+          )
+        );
         const response = await props.postInfo.request;
         switch (response?.status) {
           case 200:
@@ -42,17 +69,16 @@ const PostItem = (props) => {
             break;
           default:
             newStatus = props.postInfo?.error;
-            if(!newStatus) {
+            if (!newStatus) {
               newStatus = "missing data";
             }
         }
 
-        if (isCancelled)
-          return;
+        if (isCancelled) return;
 
         setPostMessage(newPostMessage);
         setStatusMessage(newStatus);
-      }
+      };
 
       if (props.postInfo.error) {
         setPostMessage("");
@@ -60,27 +86,30 @@ const PostItem = (props) => {
       } else {
         getPostMessage();
       }
-      return () => isCancelled = true;
+      return () => (isCancelled = true);
     }
-    
   }, [props.postInfo]);
 
   return (
     <div className="postItem">
       <div className="postLayout">
-      <img className="profileImage" src={imgSrc} alt="ProfileImage" />
+        <img className="profileImage" src={imgSrc} alt="ProfileImage" />
         <div>
           <div className="postOwnerRow">
             <Link to={`/users/${props.postInfo.owner}`}>{ownerName}</Link>
-            <span className="gray"> <span className="handle">{ownerHandle}</span> • </span>
+            <span className="gray">
+              {" "}
+              <span className="handle">{ownerHandle}</span> •{" "}
+            </span>
             <time>{getPostTime(props.postInfo.timestamp)}</time>
           </div>
           <div className="postRow">
+            {renderTopic(props.postInfo.topic)}
             {props.postInfo.message || postMessage}
             {statusMessage && <div className="status"> {statusMessage}</div>}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
